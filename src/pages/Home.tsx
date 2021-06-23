7f1c70a103bba214f48ba4c7dab5c50353c56544
Deivison Cardoso <deivisondc@gmail.com>
2021-06-22 22:42:54 -0300

    create auth context / hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,25 @@
 import { useHistory } from 'react-router-dom';
 
-import { firebase, auth } from '../services/firebase';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+import { useAuth } from '../hooks/useAuth';
+
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { signInWithGoogle, user } = useAuth();
 
   function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result)
-      history.push('/rooms/new');
-    })
+    if (!user) {
+      signInWithGoogle();
+    }
+    
+    history.push('/rooms/new');
 
   }
 
