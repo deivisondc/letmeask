33f4898c3e2e6bbdba8cc86d4ab26ed965002619
Deivison Cardoso <deivisondc@gmail.com>
2021-06-24 22:38:05 -0300

    add close room feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,6 +40,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Room is closed');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`)
   }
 
